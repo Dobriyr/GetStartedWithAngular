293331d329e56677ce9956e3e975b23f0322800d
roman kuzmich <roman.kuzmich@gmail.com>
2022-10-26 11:53:07 +0300

    ImplementApi

---
+++ PaymentAPI/PaymentAPI/Models/PaymentDetailContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PaymentAPI.Models
+{
+    public class PaymentDetailContext: DbContext
+    {
+        public PaymentDetailContext() : base()
+        {
+
+        }
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options) : base(options)
+        {
+        }
+
+       
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+
+    }
+}
